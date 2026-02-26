0bc55eff40c6fffb50e57ea386fbcdf3fb55ee18
Mohammed Ghunaim <mohammed.w.ghunaim@gmail.com>
2026-02-26 19:54:43 +0300

    create /translate endpoint

--- Backend/app.py
+++ Backend/app.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+from googletrans import Translator
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/")
-def hello_world():
-  return "<p>Hello, World!</p>"
+@app.route("/translate", methods=['POST'])
+def translate():
+  data = request.get_json()
+  text = data.get("text")
+  source_lang = data.get("source")
+  target_lang = data.get("target")
+
+  translator = Translator()
+  translated = translator.translate(text, src=source_lang, dest=target_lang)
+
+  return {"translated_text": translated.text}
+
+if __name__ == "__main__":
+  app.run(debug=True)
